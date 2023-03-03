6161663a375a4e4564a2763bb193cdd74f73475b
cfjohnny <124010753+cfjohnny@users.noreply.github.com>
2023-03-02 21:17:19 -0600

    Base para el proyecto Cajero Automatico

---
+++ Proyecto_Cajero.py
@@ -0,0 +1,19 @@
+
+while True:
+    # menu principal
+    print("1.Registrar nuevo usuario")
+    intentos_1 = int(0)
+    print("2.Usuario registrado")
+    print("3.Configuración avanzada")
+    print("4.Salir")
+    opcion = int(input())
+    if opcion == 1: # opcion para registrase como nuevo usuario
+        print()
+    elif opcion == 2: # opcion para usuario registrado
+        print()
+    elif opcion == 3: # opcion para realizar la configuracion avanzada
+        print()
+    elif opcion == 4: # opcion para salir del sistema
+        break
+    else: 
+        print("Seleccione una opción valida")
