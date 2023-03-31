9d9cc8ca3ed3dcef6e2b518ff3b4ad715a5f5b37
MelZinke <melazv1210@gmail.com>
2023-03-30 23:31:13 -0600

    logica y corregir errores

---
+++ constantes.py
@@ -0,0 +1,24 @@
+maximoIntentos = 3
+
+# constantes para el manejo de archivos
+carpetaUsuarios = 'usuarios'
+
+# opciones del menu principal
+registrarUsuario = '1'
+usuarioRegistrado = '2'
+configuracionAvanzada = '3'
+salirPrincipal = '4'
+
+# Opciones del menu Usuario Registrado
+retirarDinero = '1'
+depositarDinero = '2'
+verSaldoActual = '3'
+# ...
+salirUsuario = '7'
+
+# Posicion de los valores del usuario
+cedula = 0
+nombre = 1
+apellido1 = 2
+apellido2 = 3
+pin = 4
