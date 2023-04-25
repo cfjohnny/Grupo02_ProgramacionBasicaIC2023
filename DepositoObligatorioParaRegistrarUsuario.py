5f536e8dae53a3f5675c0bdc1b7360d215f7a3a7
Bmow1402 <pmendez80528@ufide.ac.cr>
2023-04-24 22:31:24 -0600

    implementar deposito obligatorio

---
+++ DepositoObligatorioParaRegistrarUsuario.py
@@ -0,0 +1,60 @@
+import archivos
+import cuentas
+import constantes as const
+#PARTE DEL DEPOSITO
+
+def depositoObligatorio(cedula):
+    tasaDeCambio = [1, 0, 0]
+    with archivos.abrirArchivo('', const.archivoConfig, 'r') as config:
+        lineas = [linea.rstrip('\n') for linea in config.readlines()]
+        tasaDeCambio[1] = float(lineas[7])
+        tasaDeCambio[2] = float(lineas[11])
+
+    intentos = 3 #El usuario solo tendra 3 intentos para digitar el monto minimo correcto
+    while intentos > 0:
+        print("Para poder ser un usuario de Global Bank Inc, debe realizar un depósito mínimo de 100,000 colones o equivalente en otra moneda.") #Imprimir un mensaje educando al usuario con el monto minimo
+        print("Ingrese el tipo de moneda a utilizar:\n1-Colones\n2-Dólares\n3-Bitcoins\n") #Desplegar el menu de las opciones 
+
+        monedas = ["colones", "dolares", "bitcoin"] #opciones de moneda disponibles.
+        indiceMoneda = -1
+        moneda = None
+
+        while True: #Se inicia un bucle while que se ejecutará mientras la variable monedaValida sea falsa.
+            try:
+                indiceMoneda = int(input()) - 1
+                moneda = monedas[indiceMoneda] # Se intenta obtener la opción de moneda seleccionada por el usuario a través de la entrada por teclado. Si se ingresa un valor no válido (que no sea 1, 2 o 3), se captura la excepción y se muestra un mensaje de error.
+                break #Si se obtiene una opción de moneda válida, se cambia el valor de la variable monedaValida a True para salir del bucle.
+            except (ValueError, IndexError): #En caso de obtener un error, para hacerlo facil de entender al usuario, se le educara imprimiendo un mensaje.
+                print("Moneda inválida, por favor intente de nuevo.")
+
+        #Se calcula el monto mínimo requerido para el depósito en la moneda seleccionada por el usuario.
+        montoMinimo = const.montoMinimo / tasaDeCambio[indiceMoneda]
+        # if moneda == "bitcoin": #Si la opción de moneda seleccionada es bitcoin, se cambia el valor del monto mínimo requerido a 0.0062.
+        #   montoMinimo = 1000 
+
+        #Se solicita al usuario que ingrese el monto que desea depositar. Se muestra en pantalla el monto mínimo requerido para el depósito en la moneda seleccionada.
+        montoADepositar = float(input(f"Ingrese el monto que desea depositar (el monto mínimo es {montoMinimo} {moneda}):"))
+        if montoADepositar < montoMinimo: #Si el monto ingresado por el usuario es menor que el monto mínimo requerido, se muestra un mensaje de error y se reduce el número de intentos en 1.
+            intentos -= 1
+            print(f"Debe depositar al menos {montoMinimo:.1f} {moneda}, por favor intente de nuevo. Le quedan {intentos} intentos.")
+            if intentos == 0:
+                print("Ha superado el número máximo de intentos permitidos. Volviendo al menu principal")
+                return False
+                #VOLVER AL PROGRAMA PRINCIPAL
+        else:
+            # Mostrar el monto a depositar y preguntar al usuario si desea continuar
+            print("¿Desea continuar con el depósito?")
+            print(f"- Monto depositado en {moneda}: {montoADepositar}")
+            continuarDeposito = input("¿Desea continuar con el depósito?\n1-Si\n2-No\n")
+            # Si el usuario no desea continuar, salir del bucle principal
+            if continuarDeposito == "1":
+                carpeta = f"{const.carpetaUsuarios}/{cedula}"
+                saldos = [0, 0, 0]
+                saldos[indiceMoneda] = montoADepositar
+                cuentas.guardarSaldos(carpeta, saldos)
+                print(f"El monto {montoADepositar:.1f} {moneda} ha sido depositado con éxito. ¡Gracias por confiar en Global Bank Inc!")
+                return True
+            elif continuarDeposito == "2":
+                print("El usuario no ha realizado el depósito.")
+                return False
+                #VOLVER AL PROGRAMA PRINCIPAL
--- Proyecto_Cajero.py
+++ Proyecto_Cajero.py
@@ -2,13 +2,15 @@ import os
 import getpass
 import archivos
 import usuarios
+import cuentas
+from usuarios import generarServiciosAleatorios 
 import configuracion as config
 import constantes as const
-
-# ----------------------------Menu principal------------------------------
+import DepositoObligatorioParaRegistrarUsuario as depositoObligatorioDelRegistro
+#----------------------------Menu principal------------------------------
 
 while True:
-    # menu principal
+    
     print("\n+========= MENU PRINCIPAL ==========+")
     print("| 1.Registrar nuevo usuario         |")
     print("| 2.Usuario registrado              |")
@@ -22,57 +24,65 @@ while True:
         # registrarUsuario(intentos_1)
         while intentos < const.maximoIntentos:  # numero de intentos para ingresar cedula correcta
             # solicitud de cedula
-            cedula = input(
-                "Ingrese su cedula\nLa cedula debe constar de 9 numeros\n")
+            cedula = input("Ingrese su cedula\nLa cedula debe constar de 9 numeros.\n") #solicitud de cedula
             if len(cedula) == 9:  # cedula correcta
                 intentos = 3
                 ruta = f"{const.carpetaUsuarios}/{cedula}"
                 print("Cedula aceptada")
                 if os.path.isfile(ruta):
                     print(
-                        f"Ya existe una cuenta asociada a la cédula {cedula}.")
+                        f"Ya existe una cuenta asociada a la cédula: {cedula}.") #Usuario digita una cedula que ya se encontraba en el sistema
                     break
-                else:
+                else: #Solicitarle el nombre y apellidos al usuario
                     nombre = input("Ingrese primer nombre\n")
                     apellido1 = input("Ingrese primer apellido\n")
                     apellido2 = input("Ingrese segundo apellido\n")
                     while True:
-                        pinIngresado = getpass.getpass(
-                            "Ingrese un PIN de 4 caracteres")
+                        pinIngresado = getpass.getpass("Ingrese un PIN de 4 caracteres.") #Solicitud del PIN
                         if len(pinIngresado) == 4:
-                            print("Pin aceptado")
+                            print("Pin aceptado.")
                             while True:
-                                pinConfirmacion = getpass.getpass(
-                                    "Confirme su PIN")
+                                pinConfirmacion = getpass.getpass("Confirme su PIN.") #Confirmacion del PIN
                                 if pinIngresado == pinConfirmacion:
-                                    archivos.crearEstructuraArchivos([
-                                        cedula,
-                                        nombre,
-                                        apellido1,
-                                        apellido2,
-                                        pinIngresado,
-                                    ])
-                                    print(f"El usuario {cedula} ha sido creado con éxito.")
-                                    break
+                                    exito = depositoObligatorioDelRegistro.depositoObligatorio(cedula)
+                                    if exito:
+                                        archivos.crearEstructuraArchivos([
+                                            cedula,
+                                            nombre,
+                                            apellido1,
+                                            apellido2,
+                                            pinIngresado,
+                                        ])
+                                        print(f"El usuario {cedula} ha sido creado con éxito.")
+                                    else:
+                                        pass
 
+                                    break
                                 elif pinIngresado != pinConfirmacion:
-                                    print("PIN no coincide")
+                                    print("PIN no coincide.")
                                 else:
                                     pass
                             break
                         else:
-                            print("Error")
-                            print("El PIN debe constar de 4 numeros")
-
+                            print("Error.")
+                            print("El PIN debe constar de 4 numeros.")
+    
             else:  # no se cumple con requerimientos de cedula
-                print("Error, no se digitaron la cantidad de caracteres requeridos")
+                intentos += 1
+                if len(cedula) > 9: # Cedula mayor de 9 digitos
+                    print("Los digitos de la cedula no pueden exceder 9 digitos, intentalo nuevamente.\n")
+                elif len(cedula) < 9: # Cedula menor de 9 digitos
+                    print("Los digitos de la cedula no pueden ser menores de 9 digitos, intentalo nuevamente.\n")
+                else:
+                    break
                 intentos = intentos + 1
                 if intentos == 3:
                     print(
                         """Se excedió el máximo de intentos
                         para ingresar un numero de cedula valido, 
-                        volviendo al menú principal"""
+                        volviendo al menú principal..."""
                     )
+ 
     elif opcion == const.usuarioRegistrado:  # opcion para usuario registrado
         listaUsuarios = usuarios.cargarUsuarios()
 
@@ -93,4 +103,4 @@ while True:
     elif opcion == const.salirPrincipal:  # opcion para salir del sistema
         break
     else:
-        print("Seleccione una opción valida")
+        print("Seleccione una opción valida.")
